e12172de5de3715adcb55516146361700c00208a
azraelgnosis <azraelgnosis@gmail.com>
2020-08-03 00:44:09 -0400

    Flatten looks good

--- flatten.py
+++ flatten.py
@@ -2,5 +2,23 @@ def flatten(dct:dict) -> dict:
     new_dict = {}
     for key, val in dct.items():
         if isinstance(val, dict):
-            pass
+            for key, val in flatten(val).items():
+                new_dict[key] = val
         new_dict[key] = val
+
+    return new_dict
+
+
+d = {
+    'a': {
+        'aye': 'ei',
+        'ah': 'a',
+        'a': 'ae',
+    },
+    'b': 'bee'
+}
+
+flat_d = flatten(d)
+print(flat_d)
+
+print("done")
